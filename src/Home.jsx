2aab596dd6230b10fb1152cf737722f03deaa3db
puepi <pmandel0001@yahoo.fr>
2025-04-30 14:26:16 +0100

    added toolbar actions feature

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import logoImage from './assets/second-logo.jpg'
 import SideBar from './components/SideBar';
+import heroImage from './assets/about-hero.png'
 
 function Home() {
   return (
@@ -12,20 +13,30 @@ function Home() {
       <div className="app-container">
         <SideBar />
         <main>
-          <section className='input-container'>
-            <i className="fa-solid fa-magnifying-glass"></i><input className='search' placeholder="Please enter your search here ..." type="text" />
-            {/* <label htmlFor="services-centraux">Services centraux <input type="radio" name="services" id="services-centraux" /></label>
-            <label htmlFor="services-deconcentres">Services déconcentrés <input type="radio" name="services" id="services-deconcentres" /></label> */}
-            <button>Filtrer la recherche</button>
-          </section>
-          <section className='toolbar-filter'>
-            <ul>
-              <li className='active'><a href=""><i className="fa-solid fa-rotate-right fa-small"></i>Eau</a></li>
-              <li><a href=""><i className="fa-solid fa-signal"></i>Electricité</a></li>
-              <li><a href=""><i className="fa-solid fa-location-dot"></i>Téléphone</a></li>
-              <li><a href=""><i className="fa-solid fa-image"></i>Internet</a></li>
-            </ul>
-          </section>
+          <div>
+            <section className='toolbar-filter'>
+              <ul className='menu'>
+                <li className='active'><a href=""><i class="fa-solid fa-faucet-drip"></i>Eau</a>
+                  <ul className='toolbar-actions'>
+                    <li className='list-actions'><a href="">Enregistrer</a></li>
+                    <li className='list-actions'><a href="">Modifier</a></li>
+                    <li className='list-actions'><a href="">Supprimer</a></li>
+                    <li className='list-actions'><a href="">Rechercher</a></li>
+                  </ul>
+                </li>
+                
+                <li><a href=""><i className="fa-solid fa-bolt list-item"></i>Electricité</a></li>
+                <li><a href=""><i className="fa-solid fa-phone list-item"></i>Téléphone</a></li>
+                <li><a href=""><i className="fa-solid fa-globe list-item"></i>Internet</a></li>
+              </ul>
+            </section>
+            <section className='input-container'>
+              <i className="fa-solid fa-magnifying-glass"></i><input className='search' placeholder="Please enter your search here ..." type="text" />
+              {/* <label htmlFor="services-centraux">Services centraux <input type="radio" name="services" id="services-centraux" /></label>
+              <label htmlFor="services-deconcentres">Services déconcentrés <input type="radio" name="services" id="services-deconcentres" /></label> */}
+              <button>Filtrer la recherche</button>
+            </section>
+          </div>
         </main>
       </div>
     </div>
